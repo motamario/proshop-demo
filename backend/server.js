57bec8b758f54cbd8ebc69eb3075e0838daeed2a
Mario Mota <rosasmario24@gmail.com>
2023-10-11 19:13:38 -0600

    Create custom error middleware

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@ import dotenv from 'dotenv';
 dotenv.config();
 import connectDB from './config/db.js';
 import productRoutes from './routes/productRoutes.js'
+import { notFound, errorHandler } from './middleware/errorMiddleware.js'
+
 //variable for a port
 const port = process.env.PORT || 5000;
 
@@ -20,6 +22,9 @@ app.get('/', (req, res) => {
 
 //get request to api products (aka route for all products) & sending a json response with the products
 app.use('/api/products', productRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
 
 //route for a single product
 //find goes through the products, and if the product id matchces the id in the url, then it returns that product.
