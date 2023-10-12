5c3ffd27638d0508fb9bb0862a0886bd5b748c54
Mario Mota <rosasmario24@gmail.com>
2023-10-12 12:18:14 -0600

    Get product details

--- frontend/src/slices/productsApiSlice.js
+++ frontend/src/slices/productsApiSlice.js
@@ -9,8 +9,14 @@ export const productsApiSlice = apiSlice.injectEndpoints({
                 url: PRODUCTS_URL,
             }), 
             keepUnusedDataFor: 5
+        }),
+        getProductDetails: builder.query({
+            query: (productId) => ({
+                url: `${PRODUCTS_URL}/${productId}`,
+            }),
+            keepUnusedDataFor: 5,
         })
     }),
 });
 
-export const { useGetProductsQuery } = productsApiSlice;
+export const { useGetProductsQuery, useGetProductDetailsQuery } = productsApiSlice;
